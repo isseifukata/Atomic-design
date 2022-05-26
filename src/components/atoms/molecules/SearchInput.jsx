ba209cdadfcbfe3f3973035dd53f889daf88df83
Issei Fukata <issay12@gmail.com>
2022-05-26 13:39:55 +0900

    Initial commit

---
+++ src/components/atoms/molecules/SearchInput.jsx
@@ -0,0 +1,11 @@
+import { PrimaryButton } from "../button/PrimaryButton";
+import "./styles.css";
+
+export const SearchInput = () => {
+  return (
+    <div>
+      <input type="text" />
+      <PrimaryButton></PrimaryButton>
+    </div>
+  );
+};
